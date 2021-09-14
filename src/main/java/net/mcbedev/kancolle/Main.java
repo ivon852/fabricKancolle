4719c86d8a85feaaafe68e50d4a64625251dc23e
ivon852 <qj985n2@protonmail.com>
2021-09-14 10:07:03 +0800

    Add basic files

--- src/main/java/net/mcbedev/kancolle/Main.java
+++ src/main/java/net/mcbedev/kancolle/Main.java
@@ -5,7 +5,7 @@ import net.mcbedev.kancolle.item.ModItems;
 
 public class Main implements ModInitializer {
 
-    public static final String MOD_ID = "kancolledmod";
+    public static final String MOD_ID = "kancollemod";
 
     @Override
     public void onInitialize() {
